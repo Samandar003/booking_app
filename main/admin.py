60452093deb30fb41224d90aff3e3da958842e3b
samandar <samandar200527@gmail.com>
2024-07-14 01:56:40 +0200

    admin has registered

---
+++ main/admin.py
@@ -0,0 +1,9 @@
+from .models import InstitutionModel, PostInstitModel, ScheduleModel
+from django.contrib import admin
+
+
+admin.site.register(InstitutionModel)
+admin.site.register(PostInstitModel)
+admin.site.register(ScheduleModel)
+
+
