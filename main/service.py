f9ee3fc3e5d1fcbbec957d1c431e45fd26db7fc2
samandar <samandar200527@gmail.com>
2024-07-14 16:08:26 +0200

    returning available hours

--- main/service.py
+++ main/service.py
@@ -1,4 +1,4 @@
-from models import ScheduleModel, InstitutionModel, PostInstitModel, WorkingHoursModel
+from .models import ScheduleModel, InstitutionModel, PostInstitModel, WorkingHoursModel
 from datetime import datetime, timedelta
 
 def show_available_hours(instit_obj, day):
@@ -7,17 +7,19 @@ def show_available_hours(instit_obj, day):
     end_time=working_day_obj.close_time
     schedule_objs=ScheduleModel.objects.filter(day=day, institution=instit_obj).order_by("created_at")
     schedule_list=[]
-    print(start_time)
-    if schedule_objs[0].start_time>start_time+timedelta(minutes=10):
-        schedule_list.append([start_time, schedule_objs[0].start_time])
-    for x in range(len(schedule_objs)-1):
-        x_end=schedule_objs[x].end_time
-        next_x_start=schedule_objs[x+1].start_time
-        if x_end+timedelta(minutes=10)<next_x_start:
-            schedule_list.append([x_end, next_x_start])
+    if len(schedule_objs)>0:
+        if schedule_objs[0].start_time>start_time+timedelta(minutes=10):
+            schedule_list.append([start_time, schedule_objs[0].start_time])
+        for x in range(len(schedule_objs)-1):
+            x_end=schedule_objs[x].end_time
+            next_x_start=schedule_objs[x+1].start_time
+            if x_end+timedelta(minutes=10)<next_x_start:
+                schedule_list.append([x_end, next_x_start])
             
-    if schedule_objs[-1].end_time+timedelta(minutes=10)<end_time:
-        schedule_list.append([schedule_objs[-1].end_time, end_time])
+        if schedule_objs.last().end_time+timedelta(minutes=10)<end_time:
+            schedule_list.append([schedule_objs.last().end_time, end_time])
+    else:
+        schedule_list.append([start_time.time(), end_time.time()])
     return schedule_list
 
 
