bb9907bbf1f4d2086606d78c03c277d505aae36e
samandar <samandar200527@gmail.com>
2024-07-14 15:26:37 +0200

    institution viewset

--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
-
-
+from serializers import InstitutionModelSerializer
+from models import InstitutionModel, PostInstitModel, WorkingHoursModel, ScheduleModel
 
 class InstitutionAllModelViewSet(ModelViewSet):
-    pass
-
+    serializer_class=InstitutionModelSerializer
+    
+    def get_queryset(self):
+        queryset=InstitutionModel.objects.all().order_by("-dislikes")
+        queryset=queryset.order_by("likes")
+        
+        
 
